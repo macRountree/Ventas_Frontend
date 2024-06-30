c93003998a5f32973815813845f799b675b6ad65
MacRountree <macoy1492@hotmail.com>
2024-06-30 15:23:45 -0700

    chore: Update gulpfile.js to use single quotes consistently

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 //Contenedor de Tareas  NO ES FRAMEWORK gulp es un automatizador de tareas (functions)
 
 //CSS
-const { src, dest, watch, parallel } = require("gulp"); //extrae la funcionalidad de gulp dentro de node modules
-const sass = require("gulp-sass")(require("sass")); //busca la dependencia sass y gulp-sass para que pueda compilar mi css
-const plumber = require("gulp-plumber");
+const {src, dest, watch, parallel} = require('gulp'); //extrae la funcionalidad de gulp dentro de node modules
+const sass = require('gulp-sass')(require('sass')); //busca la dependencia sass y gulp-sass para que pueda compilar mi css
+const plumber = require('gulp-plumber');
 
 //Imagenes
-const cache = require("gulp-cache");
-const imagemin = require("gulp-imagemin");
-const webp = require("gulp-webp");
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css(done) {
-  src("src/scss/**/*.scss") //una vez identifica el source ejecuta el pipe.. se puede jecutar en cadena
+  src('src/scss/**/*.scss') //una vez identifica el source ejecuta el pipe.. se puede jecutar en cadena
     .pipe(plumber())
     .pipe(sass()) //Compilamos
-    .pipe(dest("assets/css")); // Destination en assets/css
+    .pipe(dest('assets/css')); // Destination en assets/css
 
   done();
 }
@@ -23,9 +23,9 @@ function image(done) {
   const options = {
     optimizationLevel: 3,
   };
-  src("src/img/**/*.{png,jpg,svg}")
+  src('src/img/**/*.{png,jpg,svg}')
     .pipe(cache(imagemin()))
-    .pipe(dest("assets/img"));
+    .pipe(dest('assets/img'));
 }
 
 function versionWebp(done) {
@@ -34,23 +34,26 @@ function versionWebp(done) {
   const options = {
     quality: 50,
   };
-  src("src/img/**/*.{png,jpg,svg}")
+  src('src/img/**/*.{png,jpg,svg}')
     .pipe(webp(options)) //busca todas las imagenes con formato png,jpg
-    .pipe(dest("assets/img"));
+    .pipe(dest('assets/img'));
 
   done();
 }
 
 function dev(done) {
-  watch("src/scss/**/*.scss", css); //escucha el watch nuestro archivo app.css para quue compile sin recargar
+  watch('src/scss/**/*.scss', css); //escucha el watch nuestro archivo app.css para quue compile sin recargar
 
   done();
 }
+const build = series(css, parallel(image, versionWebp));
 
 exports.css = css;
 exports.image = image;
 exports.versionWebp = versionWebp;
 exports.dev = parallel(image, versionWebp, dev);
+exports.build = build;
+exports.default = build;
 
 /** Instalar despues de terminar el proyectos
  * Autoprefixer: es para que el css soporte en todos los navegadores
